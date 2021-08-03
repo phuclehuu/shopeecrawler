deef22558bb6766eec0d06d2e3219e4cc10c393a
Phuc Le <lhphuc@ecpay.vn>
2021-08-03 09:36:51 +0700

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -11,6 +11,11 @@ secret_key_base =
     You can generate one by calling: mix phx.gen.secret
     """
 
+config :shopeecrawler, ShopeeCrawler.Repo,
+  ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
 config :shopeecrawler, ShopeecrawlerWeb.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
--- lib/shopeecrawler_web/endpoint.ex
+++ lib/shopeecrawler_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule ShopeecrawlerWeb.Endpoint do
   ]
 
   socket "/socket", ShopeecrawlerWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
