0e5e9a502a0f9d84fdf969ad956166282ce99a9d
Phuc Le <lhphuc@ecpay.vn>
2021-08-03 09:39:51 +0700

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,17 +4,24 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
+database_url =
+  System.get_env("DATABASE_URL") ||
     raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
     """
 
 config :shopeecrawler, ShopeeCrawler.Repo,
   ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
 
 config :shopeecrawler, ShopeecrawlerWeb.Endpoint,
   http: [
