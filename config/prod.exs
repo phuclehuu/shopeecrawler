173fbd2f28b7e9df2b9ea7b5d04585f319e0b41d
Phuc Le <lhphuc@ecpay.vn>
2021-08-03 14:01:56 +0700

    Update cache_static_manifest config

--- config/prod.exs
+++ config/prod.exs
@@ -12,8 +12,8 @@ use Mix.Config
 config :shopeecrawler, ShopeecrawlerWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "shopeecrawler.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
-  #cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
