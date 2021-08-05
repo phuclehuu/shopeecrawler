2293f75076167a601f166e36e93051ef6a32b3e4
Phuc Le <lhphuc@ecpay.vn>
2021-08-05 15:40:18 +0700

    Implement search function

--- test/cache/simple_cache_test.exs
+++ test/cache/simple_cache_test.exs
@@ -3,6 +3,27 @@ defmodule SimpleCacheTest do
   doctest SimpleCache
 
   test "set/2 set cache value" do
-    assert SimpleCache.set(:key, :value) == :ok
+    key = :key
+    assert SimpleCache.set(key, :value, 5) == :ok
+
+    #Wait a bit for set cache already finished
+    Process.sleep(1)
+
+    assert SimpleCache.get(%{:table => :simple_cache}, key) == {:ok, :value}
+  end
+
+  test "get/2 get cache value" do
+    key = :key2
+
+    #Before set: get return {:error, :not_found}
+    assert SimpleCache.get(%{:table => :simple_cache}, key) == {:error, :not_found}
+
+    SimpleCache.set(key, :value, 5)
+
+    #Wait a bit for set cache already finished
+    Process.sleep(1)
+
+    #After set: get return {:ok, :value}
+    assert SimpleCache.get(%{:table => :simple_cache}, key) == {:ok, :value}
   end
 end
--- test/shopeecrawler/shopee_spider_test.exs
+++ test/shopeecrawler/shopee_spider_test.exs
@@ -2,7 +2,8 @@ defmodule ShopeeSpiderTest do
   use ExUnit.Case
   doctest ShopeeSpider
 
-  test "fetch/1 crawling products" do
+  test "fetch/2 crawling products" do
     assert ShopeeSpider.fetch("https://shopee.vn/api/v4/search/search_items?by=pop&limit=30&match_id=88201679&newest=0&order=desc&page_type=shop&scenario=PAGE_OTHERS&version=2") |> is_list()
+    assert ShopeeSpider.fetch("https://shopee.vn/api/v4/search/search_items?by=pop&limit=30&match_id=88201679&newest=0&order=desc&page_type=shop&scenario=PAGE_OTHERS&version=2", %{query: :apple}) |> is_list()
   end
 end
