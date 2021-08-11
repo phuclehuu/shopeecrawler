d8daeccd6023a8e2537b796d81e341a15d05fe19
Phuc Le <lhphuc@ecpay.vn>
2021-08-11 19:59:20 +0700

    Implement category menu

--- lib/shopeecrawler_web/controllers/page_controller.ex
+++ lib/shopeecrawler_web/controllers/page_controller.ex
@@ -6,10 +6,18 @@ defmodule ShopeecrawlerWeb.PageController do
   def index(conn, params) do
     shop_categoryids = get_in(params, ["shop_categoryids"])
 
-    items = SimpleCache.get(ShopeeSpider, :fetch, ["#{@url}&shop_categoryids=#{shop_categoryids}", params], ttl: 10)
+    items = SimpleCache.get(ShopeeSpider, :fetch, ["#{@url}&shop_categoryids=#{shop_categoryids}", params], ttl: 60)
 
-    categories = SimpleCache.get(ShopeeSpider, :fetch_cats, [@url_category], ttl: 60)
+    categories = SimpleCache.get(ShopeeSpider, :fetch_cats, [@url_category], ttl: 120)
 
-    render(conn, "index.html", data: %{categories: categories, items: items})
+    render(conn, "index.html", data: %{categories: categories, items: items, active: "home"})
+  end
+
+  def category(conn, %{"id" => shop_categoryids}) do
+    items = SimpleCache.get(ShopeeSpider, :fetch, ["#{@url}&shop_categoryids=#{shop_categoryids}", %{}], ttl: 60)
+
+    categories = SimpleCache.get(ShopeeSpider, :fetch_cats, [@url_category], ttl: 120)
+
+    render(conn, "index.html", data: %{categories: categories, items: items, active: shop_categoryids})
   end
 end
