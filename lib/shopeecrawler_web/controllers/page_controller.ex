ac99465436562994c5ee9b879a15269db3db9adb
phuclehuu <l.phuc@hotmail.com>
2021-09-08 09:07:39 +0700

    Revert "Implement category menu"

--- lib/shopeecrawler_web/controllers/page_controller.ex
+++ lib/shopeecrawler_web/controllers/page_controller.ex
@@ -6,18 +6,10 @@ defmodule ShopeecrawlerWeb.PageController do
   def index(conn, params) do
     shop_categoryids = get_in(params, ["shop_categoryids"])
 
-    items = SimpleCache.get(ShopeeSpider, :fetch, ["#{@url}&shop_categoryids=#{shop_categoryids}", params], ttl: 60)
+    items = SimpleCache.get(ShopeeSpider, :fetch, ["#{@url}&shop_categoryids=#{shop_categoryids}", params], ttl: 10)
 
-    categories = SimpleCache.get(ShopeeSpider, :fetch_cats, [@url_category], ttl: 120)
+    categories = SimpleCache.get(ShopeeSpider, :fetch_cats, [@url_category], ttl: 60)
 
-    render(conn, "index.html", data: %{categories: categories, items: items, active: "home"})
-  end
-
-  def category(conn, %{"id" => shop_categoryids}) do
-    items = SimpleCache.get(ShopeeSpider, :fetch, ["#{@url}&shop_categoryids=#{shop_categoryids}", %{}], ttl: 60)
-
-    categories = SimpleCache.get(ShopeeSpider, :fetch_cats, [@url_category], ttl: 120)
-
-    render(conn, "index.html", data: %{categories: categories, items: items, active: shop_categoryids})
+    render(conn, "index.html", data: %{categories: categories, items: items})
   end
 end
